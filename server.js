ea11db224c4924c4bd4ff73b19a9029231aac9ff
rohit721330 <rohithalder721@gmail.com>
2026-08-12 22:53:36 +0530

    Clean up server routes by removing redundancies
    Removed redundant routes for index, home, admin, and dashboard.

--- server.js
+++ server.js
@@ -14,22 +14,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/admin', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
-});
-
-app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
-});
-
 // API Routes
 app.use('/api', require('./api/verify'));
 
